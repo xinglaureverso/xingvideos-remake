badee9db052c3169069b076c2f12bdf13d6f885a
xinglau reverso <jaxjaxjax016@gmail.com>
2025-08-02 18:32:15 -0300

    Create shorts.ts

---
+++ js/shorts.ts
@@ -0,0 +1,15 @@
+type ShortData = {
+  slug: string;
+  title: string;
+  file: string;
+  username: string;
+  music: string;
+};
+
+type Comment = {
+  user: string;
+  message: string;
+  time: string;
+};
+
+// Futuro: lista de comentários e postagem via input com localStorage
