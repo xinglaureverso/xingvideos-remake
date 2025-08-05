eb2ff840c659487e80b405473412363be573be5a
xinglau reverso <jaxjaxjax016@gmail.com>
2025-08-05 13:09:58 -0300

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,6 @@
+function login() {
+  const user = document.getElementById("loginUser").value;
+  const pass = document.getElementById("loginPass").value;
+  const status = document.getElementById("status");
+
+  if (users
