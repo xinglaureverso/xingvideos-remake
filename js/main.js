99fc696259a6739dd515188eb9ec1ee31106839f
xinglau reverso <jaxjaxjax016@gmail.com>
2025-08-07 14:28:11 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,3 +23,23 @@ self.addEventListener('fetch', () => {});
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js');
 }
+let deferredPrompt;
+const installBtn = document.getElementById('install-btn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installBtn.style.display = 'inline-block';
+});
+
+installBtn.addEventListener('click', () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('Usuário aceitou instalar');
+      }
+      deferredPrompt = null;
+    });
+  }
+});
